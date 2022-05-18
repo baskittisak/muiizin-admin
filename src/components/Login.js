a19fab21a3a4a0fcc02cddb051ad27786c00cc19
baskittisak <basbubie.kittisak@gmail.com>
2022-05-18 22:02:27 +0700

    [EDIT] Set Show Password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 import styled from "styled-components";
 import { Box } from "../style/common";
 import BaseImage from "../center_components/BaseImage";
@@ -9,10 +9,11 @@ import muiizin_logo_auth from "../assets/image/muiizin_logo_auth.png";
 import { ReactComponent as email_icon } from "../assets/icons/email.svg";
 import { ReactComponent as key_icon } from "../assets/icons/key.svg";
 import { Input, Space } from "antd";
+import { EyeOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
 
 const Container = styled(Box)`
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - 65px);
   background-color: #fff4d6;
 `;
 
@@ -32,7 +33,26 @@ const InputContainer = styled(Input)`
   border: none;
   font-size: 24px;
 
-  :focus {
+  .ant-input {
+    background-color: #f6f9f6;
+    border: none;
+    height: 50px;
+    font-size: 24px;
+  }
+
+  .ant-input-group-addon {
+    font-size: 18px;
+    background-color: transparent;
+    border: none;
+    padding: 5px 11px 0;
+
+    > span {
+      cursor: pointer;
+    }
+  }
+
+  :focus,
+  .ant-input:focus {
     outline: none;
     box-shadow: none;
   }
@@ -42,45 +62,85 @@ const ForgetPassword = styled(Box)`
   margin-top: 10px;
 `;
 
+const Footer = styled(Box)`
+  width: 100%;
+  height: 65px;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)),
+    #fff4d6;
+  padding: 0 50px;
+`;
+
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSetShowPassword = useCallback(() => {
+    setShowPassword((prev) => !prev);
+  }, []);
+
+  const inputPassword = useMemo(
+    () => (
+      <InputContainer
+        type={showPassword ? "text" : "password"}
+        addonAfter={
+          showPassword ? (
+            <EyeInvisibleOutlined onClick={onSetShowPassword} />
+          ) : (
+            <EyeOutlined onClick={onSetShowPassword} />
+          )
+        }
+      />
+    ),
+    [showPassword, onSetShowPassword]
+  );
+
   return (
-    <Container direction="column" justify="center" align="center">
-      <BaseImage src={muiizin_logo_auth} />
-      <FormContainer direction="vertical" size={25}>
-        <div>
-          <Box align="baseline">
-            <IconSvg src={email_icon} fontSize={16} margin="0 8px 0 0" />
-            <Typography fontSize={24} lineHeight={26} color="#828282">
-              อีเมล์
-            </Typography>
-          </Box>
-          <InputContainer type="email" />
-        </div>
-        <div>
-          <Box align="baseline">
-            <IconSvg src={key_icon} fontSize={16} margin="0 8px 0 0" />
-            <Typography fontSize={24} lineHeight={26} color="#828282">
-              รหัสผ่าน
-            </Typography>
-          </Box>
-          <InputContainer type="password" />
-          <ForgetPassword direction="column" align="flex-end">
-            <Typography lineHeight={17} color="#828282" underline>
-              ลืมรหัสผ่าน?
-            </Typography>
-          </ForgetPassword>
-        </div>
-        <BaseButton
-          width="100%"
-          height={50}
-          fontSize={20}
-          bgColor="#044700"
-          color="#FFF"
-        >
-          เข้าสู่ระบบ
-        </BaseButton>
-      </FormContainer>
-    </Container>
+    <>
+      <Container direction="column" justify="center" align="center">
+        <BaseImage src={muiizin_logo_auth} />
+        <FormContainer direction="vertical" size={25}>
+          <div>
+            <Box align="baseline">
+              <IconSvg src={email_icon} fontSize={16} margin="0 8px 0 0" />
+              <Typography fontSize={24} lineHeight={26} color="#828282">
+                อีเมล์
+              </Typography>
+            </Box>
+            <InputContainer type="email" />
+          </div>
+          <div>
+            <Box align="baseline">
+              <IconSvg src={key_icon} fontSize={16} margin="0 8px 0 0" />
+              <Typography fontSize={24} lineHeight={26} color="#828282">
+                รหัสผ่าน
+              </Typography>
+            </Box>
+            {inputPassword}
+            <ForgetPassword direction="column" align="flex-end">
+              <Typography lineHeight={17} color="#828282" underline>
+                ลืมรหัสผ่าน?
+              </Typography>
+            </ForgetPassword>
+          </div>
+          <BaseButton
+            width="100%"
+            height={50}
+            fontSize={20}
+            bgColor="#044700"
+            color="#FFF"
+          >
+            เข้าสู่ระบบ
+          </BaseButton>
+        </FormContainer>
+      </Container>
+      <Footer justify="space-between" align="center">
+        <Typography fontSize={14} lineHeight={15} color="#044700">
+          COPYRIGHT 2022, Muiizin Co., Ltd
+        </Typography>
+        <Typography fontSize={14} lineHeight={15} color="#044700">
+          v 1.0.0
+        </Typography>
+      </Footer>
+    </>
   );
 };
 
