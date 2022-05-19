83a83ebef00384e0db5fab9e2176ad89242c9e39
baskittisak <basbubie.kittisak@gmail.com>
2022-05-19 23:45:00 +0700

    [ADD] Dropdown Filter

---
+++ src/center_components/filter/Dropdown.js
@@ -0,0 +1,53 @@
+import { memo, useMemo } from "react";
+import styled from "styled-components";
+import Typography from "../Typography";
+import IconSvg from "../IconSvg";
+import { ReactComponent as arrow_icon } from "../../assets/icons/arrow_down.svg";
+import { Space, Menu, Dropdown } from "antd";
+import { Box } from "../../style/common";
+
+const DropdownContainer = styled(Dropdown)`
+  cursor: pointer;
+  width: 200px;
+  border: 1px solid #d9d9d9;
+  padding: 4px 11px;
+  border-radius: 5px;
+`;
+
+const DropdownFilter = ({ label, menuItems, value, onChange }) => {
+  const menuList = useMemo(
+    () => (
+      <Menu
+        selectedKeys={[value]}
+        items={menuItems}
+        onClick={(e) => onChange(e.key)}
+      />
+    ),
+    [value, menuItems, onChange]
+  );
+
+  const menuSelected = useMemo(() => {
+    return menuItems?.find((item) => item?.key === value)?.label;
+  }, [menuItems, value]);
+
+  return (
+    <Space direction="vertical" size={5}>
+      <Typography
+        fontSize={18}
+        lineHeight={20}
+        fontWeight={700}
+        color="#828282"
+      >
+        {label}
+      </Typography>
+      <DropdownContainer overlay={menuList}>
+        <Box align="center" justify="space-between">
+          <Typography color="rgba(0, 0, 0, 0.85)">{menuSelected}</Typography>
+          <IconSvg src={arrow_icon} fontSize={15} />
+        </Box>
+      </DropdownContainer>
+    </Space>
+  );
+};
+
+export default memo(DropdownFilter);
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,9 +1,60 @@
-import { memo } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 import Frame from "../../center_components/Frame";
 import BaseButton from "../../center_components/BaseButton";
 import Search from "../../center_components/filter/Search";
+import Dropdown from "../../center_components/filter/Dropdown";
+import { Space } from "antd";
 
 const ProductList = () => {
+  const [filters, setFilters] = useState({
+    search: "",
+    category: "1",
+    status: "1",
+  });
+
+  const onFilters = useCallback((type, value) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      [type]: value,
+    }));
+  }, []);
+
+  const categoriesItems = useMemo(
+    () => [
+      {
+        key: "1",
+        label: "ทั้งหมด",
+      },
+      {
+        key: "2",
+        label: "2nd menu item",
+      },
+      {
+        key: "3",
+        label: "3rd menu item",
+      },
+    ],
+    []
+  );
+
+  const statusItems = useMemo(
+    () => [
+      {
+        key: "1",
+        label: "ทั้งหมด",
+      },
+      {
+        key: "2",
+        label: "2nd menu item",
+      },
+      {
+        key: "3",
+        label: "3rd menu item",
+      },
+    ],
+    []
+  );
+
   return (
     <Frame
       label="รายการสินค้า"
@@ -13,7 +64,25 @@ const ProductList = () => {
         </BaseButton>
       }
     >
-      <Search label="ชื่อสินค้า" onChange={(value) => console.log(value)} />
+      <Space size={25}>
+        <Search
+          label="ชื่อสินค้า"
+          value={filters.search}
+          onChange={(value) => onFilters("search", value)}
+        />
+        <Dropdown
+          label="หมวดหมู่สินค้า"
+          menuItems={categoriesItems}
+          value={filters.category}
+          onChange={(value) => onFilters("category", value)}
+        />
+        <Dropdown
+          label="สถานะ"
+          menuItems={statusItems}
+          value={filters.status}
+          onChange={(value) => onFilters("status", value)}
+        />
+      </Space>
     </Frame>
   );
 };
