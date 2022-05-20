4355e07173a553284f2fbe699602b66d07cdc0f5
baskittisak <basbubie.kittisak@gmail.com>
2022-05-20 16:30:16 +0700

    [ADD] Step Product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,11 +1,28 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import FormWrapper from "../../center_components/FormWrapper";
 import Frame from "../../center_components/Frame";
+import StepsProduct from "./StepsProduct";
 
 const Product = () => {
   const navigate = useNavigate();
+  const [current] = useState(0);
 
-  return <Frame label="เพิ่มสินค้าใหม่" onBack={() => navigate("/")}></Frame>;
+  // const onNext = useCallback(() => {
+  //   setCurrent((prev) => prev + 1);
+  // }, []);
+
+  // const onBack = useCallback(() => {
+  //   setCurrent((prev) => prev - 1);
+  // }, []);
+
+  return (
+    <Frame label="เพิ่มสินค้าใหม่" onBack={() => navigate("/")}>
+      <FormWrapper>
+        <StepsProduct current={current} />
+      </FormWrapper>
+    </Frame>
+  );
 };
 
 export default memo(Product);
---
+++ src/components/product/StepsProduct.js
@@ -0,0 +1,60 @@
+import { memo } from "react";
+import { Steps } from "antd";
+import styled from "styled-components";
+
+const { Step } = Steps;
+
+const StepsContainer = styled(Steps)`
+  .ant-steps-item-process > .ant-steps-item-container > .ant-steps-item-icon {
+    background: #fff4d6;
+    border-color: #fff4d6;
+  }
+
+  .ant-steps-item-wait .ant-steps-item-icon {
+    border-color: #bdbdbd;
+  }
+
+  .ant-steps-item-process
+    > .ant-steps-item-container
+    > .ant-steps-item-icon
+    .ant-steps-icon,
+  .ant-steps-item-process
+    > .ant-steps-item-container
+    > .ant-steps-item-content
+    > .ant-steps-item-title {
+    color: #584207;
+    font-weight: 700;
+  }
+
+  .ant-steps-item-wait .ant-steps-item-icon > .ant-steps-icon,
+  .ant-steps-item-wait
+    > .ant-steps-item-container
+    > .ant-steps-item-content
+    > .ant-steps-item-title {
+    color: #bdbdbd;
+    font-weight: 700;
+  }
+
+  .ant-steps-item-process
+    > .ant-steps-item-container
+    > .ant-steps-item-tail::after,
+  .ant-steps-item-wait
+    > .ant-steps-item-container
+    > .ant-steps-item-tail::after {
+    background-color: #bdbdbd;
+  }
+`;
+
+const steps = ["ข้อมูลสินค้า", "ตัวเลือกและรูปภาพ", "รายละเอียดสินค้า"];
+
+const StepsProduct = ({ current }) => {
+  return (
+    <StepsContainer current={current} labelPlacement="vertical">
+      {steps.map((step) => (
+        <Step key={step} title={step} />
+      ))}
+    </StepsContainer>
+  );
+};
+
+export default memo(StepsProduct);
