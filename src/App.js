382da757d3cf6f96a7c16169018bb22d51548f44
baskittisak <basbubie.kittisak@gmail.com>
2022-05-30 22:31:14 +0700

    [EDIT] Config Provider Locale

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { memo } from "react";
+import { ConfigProvider } from "antd";
 import Layout from "./center_components/Layout";
 import PageRoutes from "./resource/PageRoutes";
+import th_TH from "antd/lib/locale/th_TH";
+import "dayjs/locale/th";
 
 const App = () => {
   return (
     <Layout>
-      <PageRoutes />
+      <ConfigProvider locale={th_TH}>
+        <PageRoutes />
+      </ConfigProvider>
     </Layout>
   );
 };
